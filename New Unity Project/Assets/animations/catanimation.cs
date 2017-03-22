e30e0058d292db1f030418c9350baa563ad42f9c
Unknown <ew1152@nyu.edu>
2017-03-22 10:00:42 -0400

    3/22 push

---
+++ New Unity Project/Assets/animations/catanimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catanimation : MonoBehaviour {
+
+	Animator catanimator;
+
+	// Use this for initialization
+	void Start () {
+		catanimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Horizontal") != 0) {
+			catanimator.Play ("cat_walk");
+		} else {
+			catanimator.Play ("cat_idle");
+		}
+	}
+}
---
+++ RelativityPlatformer/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioSource musicMain;
+	public AudioSource musicBkgd;
+	public AudioSource musicLight;
+
+	public AudioClip musicMainIntro;
+	public AudioClip musicMainLoop;
+	public AudioClip musicBkgdIntro;
+	public AudioClip musicBkgdLoop;
+	public AudioClip musicLightIntro;
+	public AudioClip musicLightLoop;
+
+	float mainAmp;
+	float bkgdAmp;
+	float lightAmp;
+
+	// Use this for initialization
+	void Start () {
+		musicMain.clip = musicMainIntro;
+		musicMain.Play ();
+		mainAmp = 1f;
+		musicBkgd.clip = musicBkgdIntro;
+		musicBkgd.Play ();
+		bkgdAmp = 1f;
+		musicLight.clip = musicLightIntro;
+		musicLight.Play ();
+		lightAmp = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!musicMain.isPlaying) {
+			musicMain.clip = musicMainLoop;
+			musicMain.Play ();
+		}
+		if (!musicBkgd.isPlaying) {
+			musicBkgd.clip = musicBkgdLoop;
+			musicBkgd.Play ();
+		}
+		if (!musicLight.isPlaying) {
+			musicLight.clip = musicLightLoop;
+			musicLight.Play ();
+		}
+		if (Mathf.Abs(Player.lightCounter) > 3 - (3 * mainAmp) && mainAmp > 0.6f) {
+			mainAmp -= 0.01f;
+		}
+		if (Mathf.Abs(Player.lightCounter) < 3 - (3 * mainAmp) && mainAmp < 1f) {
+			mainAmp += 0.01f;
+		}
+		if (Mathf.Abs(Player.lightCounter) > 3 - (3 * bkgdAmp)) {
+			bkgdAmp -= 0.01f;
+		}
+		if (Mathf.Abs(Player.lightCounter) < 3 - (3 * bkgdAmp)) {
+			bkgdAmp += 0.01f;
+		}
+		if (Mathf.Abs(Player.lightCounter) > 3 * lightAmp) {
+			lightAmp += 0.01f;
+		}
+		if (Mathf.Abs(Player.lightCounter) < 3 * lightAmp) {
+			lightAmp -= 0.01f;
+		}
+		musicMain.volume = mainAmp;
+		musicBkgd.volume = bkgdAmp;
+		musicLight.volume = lightAmp;
+	}
+}
